0dd5388475f233981f200f9db90ae421cd1d47c2
Willian Felipe <w.felipebraz@gmail.com>
2022-03-23 19:00:37 -0300

    feito questao 9

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let lista = document.getElementById('lista-tarefas')
-let botao = document.getElementById('criar-tarefa')
-let inputElementos = document.getElementById('texto-tarefa')
-let elementos = document.getElementsByTagName('li')
-let registrado = ''
+let lista = document.getElementById('lista-tarefas');
+let botao = document.getElementById('criar-tarefa');
+let inputElementos = document.getElementById('texto-tarefa');
+let elementos = document.getElementsByTagName('li');
+let registrado = '';
+let apagador = document.getElementById('apaga-tudo')
 
 botao.addEventListener('click', adicionaElemento);
 
@@ -30,5 +31,37 @@ if (elementos[index].className.includes('selected')){
 event.target.classList.add('selected'); 
 }   
 
+lista.addEventListener('dblclick', doubleClick)
+function doubleClick(event){
+
+    if(event.target.className.includes('completed')){
+        event.target.classList.remove('completed')
+    }
+    else if (event.target.className.includes('')){
+        event.target.classList.add('completed')
+    }
+   
 
 
+
+
+}
+// lista.addEventListener('dblclick', doubleClick2)
+// function doubleClick2(event){
+//     for (index = 0; index < elementos.length; index += 1){
+//         if(event.target.className.includes('completed')){
+//             event.target.classList.remove('completed')
+//         }
+
+
+// }
+// } 
+
+apagador.addEventListener('click', apagaCoisas)
+function apagaCoisas(){
+for(let index2 = elementos.length - 1; index2 >= elementos.length; index -= 1){
+     lista.removeChild(elementos[index2])
+    }
+
+
+}
